2a4cf6fba95cc3a0bd7280d3a9bdfcde8816738a
iharshchauhan <harshpratapchauhan@gmail.com>
2026-03-07 22:39:35 +0530

    gtm fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,10 +31,29 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MCTJXL5C');`,
+          }}
+        />
+      </head>
       <body
         className={`${nunitoSans.variable} ${cormorantGaramond.variable} antialiased min-h-screen flex flex-col selection:bg-primary selection:text-primary-foreground`}
         suppressHydrationWarning
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MCTJXL5C"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <AnimatedBackground />
         <Suspense fallback={null}>
           <Navigation />
