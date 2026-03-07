db37faeb8d62ee0d8746927ef8a8d6d2876c29f7
iharshchauhan <harshpratapchauhan@gmail.com>
2026-03-07 23:12:27 +0530

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,15 @@ export const metadata: Metadata = {
   title: "harshc_ | Product & Growth",
   description:
     "Product/Growth enthusiast and an amateur human figuring out both the world and the web.",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
+      { url: "/favicon-16x16.png", type: "image/png", sizes: "16x16" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
